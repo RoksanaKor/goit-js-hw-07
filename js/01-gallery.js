7c6c3aa019b72a784098c5b01c808cfa23111f82
RoksanaKor <roksanakorban@gmail.com>
2024-02-09 21:23:43 +0100

    finished tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
 createGallery();
 function createGallery() {
- let newGallery =[];
- for (let i=0; i<galleryItems.length; i++) {
-  let imageDiv = document.createElement("div");
-  imageDiv.classList.add("gallery__item");
-  const imageLink = document.createElement("a");
-  imageLink.setAttribute("href", galleryItems[i].original);
-  imageLink.classList.add("gallery__link");
-  imageDiv.appendChild(imageLink);
-  const imageElement = document.createElement("img");
-  imageElement.classList.add("gallery__image");
-  imageElement.setAttribute("src", galleryItems[i].preview);
-  imageElement.setAttribute("data-source", galleryItems[i].original);
-  imageElement.setAttribute("alt", galleryItems[i].description);
-  imageLink.appendChild(imageElement);
-  newGallery.push(imageDiv);
- }
- gallery.append(...newGallery);
+  let newGallery = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const listElement = document.createElement("li");
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("gallery__item");
+    const imageLink = document.createElement("a");
+    imageLink.setAttribute("href", galleryItems[i].original);
+    imageLink.classList.add("gallery__link");
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("gallery__image");
+    imageElement.setAttribute("src", galleryItems[i].preview);
+    imageElement.setAttribute("data-source", galleryItems[i].original);
+    imageElement.setAttribute("alt", galleryItems[i].description);
+    listElement.appendChild(imageDiv);
+    imageDiv.appendChild(imageLink);
+    imageLink.appendChild(imageElement);
+    newGallery.push(imageDiv);
+  }
+  gallery.append(...newGallery);
 }
 
 gallery.addEventListener("click", (event) => {
- event.preventDefault();
- if (event.target.nodeName !== "IMG") {
-  return;
- }
- const instance = basicLightbox.create(`
-    <img src= "${event.target.dataset.source}"></img>`
-)
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src= "${event.target.dataset.source}"></img>`);
 
-instance.show();
-
-})
-
-
-
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+
+//ALTERNATIVE CODE
+//
+// createGallery();
+// function createGallery() {
+//   let newGallery = [];
+//   for (let i = 0; i < galleryItems.length; i++) {
+//     const listElement = document.createElement("li");
+//     const imageLink = document.createElement("a");
+//     imageLink.setAttribute("href", galleryItems[i].original);
+//     imageLink.classList.add("gallery__item");
+//     const imageElement = document.createElement("img");
+//     imageElement.classList.add("gallery__image");
+//     imageElement.setAttribute("src", galleryItems[i].preview);
+//     imageElement.setAttribute("alt", galleryItems[i].description);
+//     listElement.appendChild(imageLink);
+//     imageLink.appendChild(imageElement);
+//     newGallery.push(listElement);
+//   }
+//   gallery.append(...newGallery);
+// }
+
+const galleryHelp = galleryItems
+  .map(
+    (value) =>
+      `<a href="${value.original}" class="gallery__item">
+        <img
+          class="gallery__image"
+          src="${value.preview}"
+          alt="${value.description}"
+        ></img>
+      </a>`
+  )
+  .join("");
+
+gallery.innerHTML = galleryHelp;
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionType: "attr",
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
